87e22ed969633978ca16a496923a4458ac513107
farhanapimatic <git@apimatic.io>
2017-10-18 09:56:39 +0000

    r

---
+++ APIMATICCalculator.PCL/Configuration.cs
@@ -0,0 +1,11 @@
+using APIMATICCalculator.PCL.Models;
+namespace APIMATICCalculator.PCL
+{
+    public partial class Configuration
+    {
+
+        //The base Uri for API calls
+        public static string BaseUri = "http://examples.apimatic.io/apps/calculator";
+
+    }
+}
